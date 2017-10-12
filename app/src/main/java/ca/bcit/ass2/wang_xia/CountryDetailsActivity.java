9fa9dc08aeb1c152568ce3084771ed6e65e456ea
eternal75 <bennywang75@hotmail.com>
2017-10-11 22:54:24 -0700

    Removed TODO

--- app/src/main/java/ca/bcit/ass2/wang_xia/CountryDetailsActivity.java
+++ app/src/main/java/ca/bcit/ass2/wang_xia/CountryDetailsActivity.java
@@ -11,7 +11,6 @@ public class CountryDetailsActivity extends AppCompatActivity {
 
     ImageView flagImage;
 
-    // TODO: Visualize Data (Must)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
