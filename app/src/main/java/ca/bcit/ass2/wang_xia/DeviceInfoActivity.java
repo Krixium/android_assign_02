d4ddbb79f68f0d5080add4e7699db7dcbfb7036e
eternal75 <bennywang75@hotmail.com>
2017-10-19 21:52:10 -0700

    Finished lab 7

---
+++ app/src/main/java/ca/bcit/ass2/wang_xia/DeviceInfoActivity.java
@@ -0,0 +1,40 @@
+package ca.bcit.ass2.wang_xia;
+
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DeviceInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_info);
+
+        TextView manufacturerTextView = (TextView) findViewById(R.id.manufacturerText);
+        TextView modelTextView = (TextView) findViewById(R.id.modelText);
+        TextView versionTextView = (TextView) findViewById(R.id.versionText);
+        TextView versionReleaseTextView = (TextView) findViewById(R.id.versionReleaseText);
+        TextView serialNumberTextView = (TextView) findViewById(R.id.serialNumberText);
+
+        Bundle info = getIntent().getExtras();
+
+        manufacturerTextView.setText(String.format(
+                getResources().getString(R.string.manufacturerLocale), Build.MANUFACTURER
+        ));
+        modelTextView.setText(String.format(
+                        getResources().getString(R.string.modelLocale), Build.MODEL
+        ));
+        versionTextView.setText(String.format(
+                getResources().getString(R.string.versionLocale), Build.VERSION.SDK_INT
+        ));
+        versionReleaseTextView.setText(String.format(
+                        getResources().getString(R.string.versionReleaseLocale), Build.VERSION.RELEASE
+        ));
+        serialNumberTextView.setText(String.format(
+                        getResources().getString(R.string.serialNumberLocale), Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID)
+        ));
+    }
+}
--- app/src/main/java/ca/bcit/ass2/wang_xia/MainActivity.java
+++ app/src/main/java/ca/bcit/ass2/wang_xia/MainActivity.java
@@ -2,8 +2,14 @@ package ca.bcit.ass2.wang_xia;
 
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -23,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.mainToolbar));
 
         List<String> regionsList;
         final ListView listView;
@@ -52,6 +60,47 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toobar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.actionShare:
+                actionShareCallback();
+                break;
+            case R.id.actionDevice:
+                actionDeviceCallback();
+                break;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void actionShareCallback() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, getResources().getString(R.string.shareSubject));
+        String shareBody = getResources().getString(R.string.shareBodyHeader) + " ";
+        for (String s : getResources().getStringArray(R.array.regionsArray)) {
+            shareBody += s + ", ";
+        }
+        shareBody = shareBody.substring(0, shareBody.length() - 2);
+        intent.putExtra(Intent.EXTRA_TEXT, shareBody);
+        startActivity(Intent.createChooser(intent, getResources().getString(R.string.sharePrompt)));
+    }
+
+    private void actionDeviceCallback() {
+        startActivity(new Intent(this, DeviceInfoActivity.class));
     }
 
     @Override
